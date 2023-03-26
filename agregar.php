fe3c6aba279f451cf792590737682d20d19c4343
yael-lopez <yael44gr@gmail.com>
2023-03-26 13:51:25 -0600

    cors

--- agregar.php
+++ agregar.php
@@ -1,7 +1,9 @@
 <?php 
 
-	header("Access-Control-Allow-Origin: *");
-	header("Access-Control-Allow-Headers: content-type");
+	header('Access-Control-Allow-Origin: *');
+	header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+	header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+	header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 
 	$jsonUser = json_decode(file_get_contents("php://input"));
 
--- registro.php
+++ registro.php
@@ -1,9 +1,7 @@
 <?php 
 
-	header('Access-Control-Allow-Origin: *');
-	header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-	header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-	header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Headers: content-type");
 
 
 	$jsonUser = json_decode(file_get_contents("php://input"));
